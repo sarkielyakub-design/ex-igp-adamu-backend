87e83da784f417b86357d50bf8084417cdc7e85d
your name <you@email.com>
2026-06-07 22:17:26 +0100

    Fix CORS for Vercel

--- app/main.py
+++ app/main.py
@@ -33,15 +33,14 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-
         "https://ex-igp-frontend-vaak.vercel.app",
-
-        "https://ex-igp-frontend-vaak-4n1jeaweu-sarkielyakub-designs-projects.vercel.app",
     ],
+    allow_origin_regex=r"https://.*\.vercel\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 # =========================
 # STATIC FILES
 # =========================
