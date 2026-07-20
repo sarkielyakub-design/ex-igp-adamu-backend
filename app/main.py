6180f76ffb3b6c56824b51378b828f1f07e28eb0
your name <you@email.com>
2026-07-20 09:55:56 +0100

    Prepare Railway deployment

--- app/main.py
+++ app/main.py
@@ -31,9 +31,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "https://ex-igp-frontend-vaak.vercel.app",
+        "https://ex-igp-frontend-vaak.vercel.app"
     ],
     allow_origin_regex=r"https://.*\.vercel\.app",
     allow_credentials=True,
